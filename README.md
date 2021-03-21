e3420e66669bc26768442b1e5f5ba103ed7da871
Vinay Kumar <vinay@zimyo.com>
2021-03-21 16:59:57 +0530

    master

---
+++ README.md
@@ -0,0 +1,17 @@
+# excel-to-json
+
+Express Multipart body data Parser: parse multipart form data to valid array consisting key value pair with file buffer.
+
+## Install
+
+```
+  npm install @krvinay/multipart-body-parser
+```
+
+## Usage
+
+```javascript
+  multipart_parser = require("@krvinay/multipart-body-parser");
+  app = require("express")();
+  app.use(multipart_parser);
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+const multer = require("multer");
+const normalized_file_name = (file_name) => {
+    let f_part = (file_name || "").split(".");
+    let ext = "";
+    if (f_part.length > 1) {
+        ext = f_part[f_part.length - 1].trim().toLowerCase();
+        delete f_part[f_part.length - 1];
+    }
+    file_name = f_part.join(" ").replace(/[^A-Za-z 0-9_]/g, " ").trim().replace(/[ ]+/g, "_").toLowerCase();
+    if (ext) {
+        file_name += "." + ext;
+    }
+    return file_name;
+}
+module.exports = multipart_body_parser;
+function multipart_body_parser(req, res, next) {
+    let upload = multer({
+        storage: multer.memoryStorage()
+    }).any();
+    upload(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            next(new Error("Unable to handle multipart form data."));
+        } else if (err) {
+            next(err);
+        } else {
+            if (!req.body) {
+                req.body = {};
+            }
+            for (const k in req.files) {
+                let f = req.files[k];
+                if (!Object.hasOwnProperty.call(req.body, f.fieldname)) {
+                    req.body[f.fieldname] = [];
+                }
+                req.body[f.fieldname].push({
+                    file_name: normalized_file_name(f.originalname),
+                    encoding: f.encoding,
+                    mimetype: f.mimetype,
+                    size: f.size,
+                    buffer: f.buffer
+                });
+                console.log(normalized_file_name(f.originalname), f.originalname)
+            }
+            delete req.files;
+            for (const k in req.body) {
+                let f = req.body[k];
+                if (typeof f == "object" && f.length == 1) {
+                    req.body[k] = req.body[k][0];
+                }
+            }
+            next();
+        }
+    });
+}
